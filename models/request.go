fc0ddc2a54debb0e26cbc32ffc2d797f6ededabb
Modifa <34087012+Modifa@users.noreply.github.com>
2022-08-23 00:36:18 +0200

    Update request.go

--- models/request.go
+++ models/request.go
@@ -1,6 +1,6 @@
 package models
 
 type DeveloperRequest struct {
-	Username string `json:username`
-	Password string `json:password`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
